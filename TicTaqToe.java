3a0dd271caaf24627bd45899109c0c7234c02b3b
sujit gargate <sujitgargate5893@gmail.com>
2020-08-07 10:08:24 +0530

    Refactored : Added choosing a letter for play

--- TicTaqToe.java
+++ TicTaqToe.java
@@ -1,15 +1,27 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class TicTaqToe {
 
-    //Variables
     Random randomToss = new Random();
+    String computerLetterOfPlay = null;
+    String playerLetterOfPlay = null;
     String playBoard[] = new String [9];
-    int playerPosition = 10;
-    int computerPosition = 10;
 
     public static void main(String[] args) {
         TicTaqToe obj = new TicTaqToe();
+import java.util.Random;
+import java.util.Scanner;
+
+public class UC3 {
+
+    Random randomToss = new Random();
+    String computerLetterOfPlay = null;
+    String playerLetterOfPlay = null;
+    String playBoard[] = new String [9];
+
+    public static void main(String[] args) {
+        UC3 obj = new UC3();
         obj.resetBoard();
     }
 
@@ -28,14 +40,50 @@ public class TicTaqToe {
         return toss;
     }
 
-    public String playerTurn() {
+    //This method chooses between computer and player.
+    public void playerTurn() {
         boolean playerTurn = playerToss();
         if(playerTurn == true) {
-            System.out.println("Player will Play");
-            return "Player";
+            System.out.println("Player will Play with letter "+playerLetterOfPlay());
         } else {
-            System.out.println("Computer will Play");
-            return "Computer";
+            System.out.println("Computer will Play with letter "+computerLetterOfPlay());
+        }
+    }
+
+    public String allocateLetter(int selectLetter) {
+        if(selectLetter == 1){
+            computerLetterOfPlay = "X";
+            return playerLetterOfPlay = "O";
+        } else {
+            computerLetterOfPlay = "O";
+            return playerLetterOfPlay = "X";
+        }
+    }
+
+    public String playerLetterOfPlay() {
+        System.out.println("Enter The Letter You Want To Play With \nX. OR O.");
+        Scanner sc = new Scanner(System.in);
+        String letterOfPlay = sc.next();
+
+        switch(letterOfPlay){
+            case "O":
+                return allocateLetter(1);
+            case "X":
+                return allocateLetter(2);
+            default:
+                System.out.println("Wrong Input");
+        }
+        return letterOfPlay;
+    }
+
+    //Choosing letter for computer Randomly.
+    public String computerLetterOfPlay() {
+
+        boolean playLetter = playerToss();
+        if(playLetter == true) {
+            return allocateLetter(1);
+        } else {
+            return allocateLetter(2);
         }
     }
 }
