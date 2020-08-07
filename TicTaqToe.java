8e9fd30a1f648ad8632b8b55d878047ea4cdc15a
sujit gargate <sujitgargate5893@gmail.com>
2020-08-07 09:50:29 +0530

    Refactored : Code For Toss For The Game

--- TicTaqToe.java
+++ TicTaqToe.java
@@ -1,7 +1,12 @@
+import java.util.Random;
+
 public class TicTaqToe {
 
     //Variables
+    Random randomToss = new Random();
     String playBoard[] = new String [9];
+    int playerPosition = 10;
+    int computerPosition = 10;
 
     public static void main(String[] args) {
         TicTaqToe obj = new TicTaqToe();
@@ -12,9 +17,25 @@ public class TicTaqToe {
     public void resetBoard() {
         for(int i = 0; i < 9; i++)
         {
-            //Allocating null to every position of array
             playBoard[i] = "\0";
         }
         System.out.println("Board Reset Successful");
+        playerTurn();
+    }
+
+    public boolean playerToss() {
+        boolean toss = randomToss.nextBoolean();
+        return toss;
+    }
+
+    public String playerTurn() {
+        boolean playerTurn = playerToss();
+        if(playerTurn == true) {
+            System.out.println("Player will Play");
+            return "Player";
+        } else {
+            System.out.println("Computer will Play");
+            return "Computer";
+        }
     }
 }
